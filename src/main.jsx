b24db4d32fc0c876f379d80b6826c410ae48dc70
hariprasatham <hariprasatham@gmail.com>
2023-07-15 13:55:21 +0530

    changed vercel analytics import statement

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics/*'
+import { inject } from '@vercel/analytics'
 inject()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
